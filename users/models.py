96fa48b61dcdbc1b1451b4645ab4212b2321a3ed
Sergey <bondarevsv36@gmail.com>
2026-01-03 18:29:04 +0300

    Откачены миграции пользователя и применена новая модель ползователя. сформированы и применены миграции

--- users/models.py
+++ users/models.py
@@ -1,5 +1,20 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 
 class CustomUser(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Адрес электронной почты")
+    avatar = models.ImageField(upload_to="users/image", blank=True, null=True, verbose_name="Аватар")
+    phone_number = models.CharField(max_length=15, blank=True, null=True, verbose_name="Номер телефона")
+    country = models.CharField(max_length=50, blank=True, null=True, verbose_name="Страна проживания")
+    token = models.CharField(max_length=100, blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
