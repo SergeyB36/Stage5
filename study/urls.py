51dbab8fc59c2c34439ad24a4f688e69165e09b6
Sergey <bondarevsv36@gmail.com>
2026-01-04 13:31:13 +0300

    Корректировка url

--- study/urls.py
+++ study/urls.py
@@ -19,11 +19,11 @@ router = SimpleRouter()
 router.register("", CourseViewSet)
 
 urlpatterns = [
-    path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
-    path("lesson/", LessonListAPIView.as_view(), name="lesson_list"),
-    path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_retrieve"),
-    path("lesson/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson_delete"),
-    path("lesson/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson_update"),
+    path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson-create"),
+    path("lesson/", LessonListAPIView.as_view(), name="lesson-list"),
+    path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson-retrieve"),
+    path("lesson/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson-delete"),
+    path("lesson/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson-update"),
 ]
 
 urlpatterns += router.urls
