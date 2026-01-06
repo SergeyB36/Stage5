c13987dd051af1c2a81515ef66bf1f79af99c724
Sergey <bondarevsv36@gmail.com>
2026-01-06 16:48:57 +0300

    Добавлено описание уроков в каждом курсе

--- study/serializers.py
+++ study/serializers.py
@@ -4,18 +4,22 @@ from rest_framework.serializers import ModelSerializer
 from study.models import Course, Lesson
 
 
-class CourseSerializer(ModelSerializer):
-    course_lesson_count = SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = ("id", "name", "description", "course_lesson_count")
-
-    def get_course_lesson_count(self, obj):
-        return obj.lessons.all().count()
-
-
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = ("id", "name", "description", "course")
+
+
+class CourseSerializer(ModelSerializer):
+    course_lesson_count = SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ("id", "name", "description", "course_lesson_count", "lessons")
+
+    def get_course_lesson_count(self, obj):
+        return obj.lessons.count()
+
+
+
